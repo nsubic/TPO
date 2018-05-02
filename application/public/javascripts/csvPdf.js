9920ab7387f6deca3a9389dec9ff5eabeea873b6
Jan <jansmi@gmail.com>
2018-05-02 13:08:27 +0000

    izvoz za st vpisanih v pdf in csv, +popravki izvoza za pdf pri seznamu vpisanih

--- application/public/javascripts/csvPdf.js
+++ application/public/javascripts/csvPdf.js
@@ -54,41 +54,91 @@ function myFunction() {
 };
 
 function demoFromHTML() {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    // source can be HTML-formatted string, or a reference
-    // to an actual DOM element from which the text will be scraped.
-    var source = $('#source')[0];
-    console.log(source)
-    // we support special element handlers. Register them with jQuery-style 
-    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-    // There is no support for any other type of selectors 
-    // (class, of compound) at this time.
-    var specialElementHandlers = {
-        // element with id of "bypass" - jQuery style selector
-        '#bypassme': function (element, renderer) {
-            // true = "handled elsewhere, bypass text extraction"
-            return true
-        }
-    };
-    var margins = {
-        top: 80,
-        bottom: 60,
-        left: 10,
-        width: 700
-    };
-    // all coords and widths are in jsPDF instance's declared units
-    // 'inches' in this case
-    pdf.fromHTML(
-    source, // HTML string or DOM elem ref.
-    margins.left, // x coord
-    margins.top, { // y coord
-        'width': margins.width, // max width of content on PDF
-        'elementHandlers': specialElementHandlers
-    },
 
-    function (dispose) {
-        // dispose: object with X, Y of the last line add to the PDF 
-        //          this allow the insertion of new lines after html
-        pdf.save('Test.pdf');
-    }, margins);
+    var source = $('#source')[0];
+
+    DocRaptor.createAndDownloadDoc("YOUR_API_KEY_HERE", {
+            test: true, // test documents are free, but watermarked
+            type: "pdf",
+            document_content: source.innerHTML, // use this page's HTML
+            // document_content: "<h1>Hello world!</h1>",               // or supply HTML directly
+            // document_url: "http://example.com/your-page",            // or use a URL
+            // javascript: true,                                        // enable JavaScript processing
+             prince_options: {
+               media: "screen",                                       // use screen styles instead of print styles
+             }
+      })
 }
+
+
+
+function myFunction1() {
+  var titles = [];
+  var data = [];
+
+  /*
+   * Get the table headers, this will be CSV headers
+   * The count of headers will be CSV string separator
+
+  /*
+   * Get the actual data, this will contain all the data, in 1 array
+   */
+  $('p').each(function() {
+    data.push($(this).text());
+  });
+  /*
+   * Convert our data to CSV string
+   */
+    var row = ""
+    for (var i = 0; i < data.length-1; i++) {
+        var cols = data[i].split(" ")
+        var size = cols.length
+        for(var j = 0; j < size; j++){
+            cols[j] = cols[j].trim()
+            if(cols[j] == "" && cols[j-1] == ""){
+                cols.splice(j,1)
+                j=0
+                size = size-1;
+            }
+            if(cols[j] == "Šifra" || cols[j] == "predmeta:" || cols[j] == "Leto" || cols[j] == "izvajanja:" || cols[j] == "Število" || cols[j] == "vpisanih:" ){
+                cols.splice(j,1)
+                j=0
+                size = size-1;
+            }
+            
+        }
+        console.log(cols)
+        for (var j = 1; j < cols.length; j++){
+            if(j!=1 && cols[j] != "") row = row + " "
+            row = row + cols[j];
+            if(cols[j] == ""){
+                 row = row +','
+                 row = row + cols[j+1]
+                 row = row +','
+                 row = row + cols[j+2]
+                 row = row +','
+                 row = row + cols[j+3]
+                 j=cols.length
+            } 
+        }
+        row=row+'\n'
+
+    }
+
+  /*
+   * Make CSV downloadable
+   */
+  var downloadLink = document.createElement("a");
+  var blob = new Blob(["\ufeff", row]);
+  console.log(blob)
+  var url = URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = "data.csv";
+
+  /*
+   * Actually download CSV
+   */
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+};
