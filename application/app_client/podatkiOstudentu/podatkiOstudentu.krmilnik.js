06b9b0166503f8a1e0f8941f4e717d0012e724cd
mkranjc <mk7017@student.uni-lj.si>
2018-05-22 20:03:50 +0000

    Client Final za Žeton

--- application/app_client/podatkiOstudentu/podatkiOstudentu.krmilnik.js
+++ application/app_client/podatkiOstudentu/podatkiOstudentu.krmilnik.js
@@ -1,10 +1,27 @@
 (function() {
   /* global angular */
   
-  podatkiOstudentu.$inject = ['$location','estudentPodatki'];
-  function podatkiOstudentu($location,estudentPodatki) {
-    
+  podatkiOstudentu.$inject = ['$scope', '$location','estudentPodatki'];
+  function podatkiOstudentu($scope, $location, estudentPodatki) {
     var vm = this;
+    
+    $scope.vrsteVpisov = [];
+    $scope.zeton = {
+      vrsta_vpisa: null,
+      vpisna_stFK: null,
+      izkoriscen: null,
+      visoko_povprecje: null,
+    }
+    
+    estudentPodatki
+      .vrsteVpisa()
+      .then(function(res) {
+        $scope.vrsteVpisov = res.data.response;
+        console.log("vrste vpisov", $scope.vrsteVpisov);
+      });
+      
+
+    
     estudentPodatki.studenti().then(
     function success(odgovor) {
       vm.sporocilo = odgovor.data.length > 0 ? "" : "Ni nobenih predmetov.";
@@ -18,6 +35,22 @@
     vm.izpis = function(vpisna,ime,priimek) {
       vm.ime=ime;
       vm.priimek=priimek;
+      
+      estudentPodatki
+        .dobiZeton(vpisna)
+        .then(function(res) {
+          console.log("zetoni", res);
+          var data = res.data.response;
+          if(data.length > 0) {
+            var datum = data[0];
+            console.info("zeton", datum);
+            $scope.zeton.vpisna_stFK = datum.vpisna_stFK;
+            $scope.selVrstaVpisa = $scope.zeton.vrsta_vpisa = datum.vrsta_vpisa;
+            $scope.zeton.izkoriscen = datum.izkoriscen === 1 ? true : false;
+            $scope.zeton.visoko_povprecje = datum.visoko_povprecje;
+          }
+        })
+      
       estudentPodatki.studentiVpis(vpisna).then(
         function success(odgovor) {
           vm.sporocilo = odgovor.data.length > 0 ? "" : "Ni nobenih studentov.";
@@ -27,6 +60,20 @@
           vm.sporocilo = "There was an error!";
           console.log(odgovor.e);
       });
+      
+      $scope.shraniZeton = function() {
+        console.log("shraniZeton");
+        var data = $scope.zeton;
+        data.izkoriscen = data.izkoriscen === true ? 1 : 0;
+        estudentPodatki
+          .updateZeton(data)
+          .then(function(res) {
+            alert("Žeton je posodobljen");
+          }, function(error) {
+            vm.sporocilo = "There was an error!";
+            console.log(error.e);
+          })
+      }
     };
   }
   
